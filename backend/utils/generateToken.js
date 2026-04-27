5a2883662083c74c6af56018f1b2cc1792427c91
dwn_raiyan <dewan.raiyan13@gmail.com>
2026-04-28 03:05:02 +0600

    Fix cross-domain

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -16,7 +16,7 @@ export const setTokenCookies = (res, accessToken, refreshToken) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
   };
 
   res.cookie('accessToken', accessToken, {
